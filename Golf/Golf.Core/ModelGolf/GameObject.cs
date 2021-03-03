e8d6a7dbc70d34f0b0587569e446d26010bb945b
nicolas63 <nicolas_raymond@orange.fr>
2021-03-03 16:59:31 +0100

    Test create bounding boxes

--- Golf/Golf.Core/ModelGolf/GameObject.cs
+++ Golf/Golf.Core/ModelGolf/GameObject.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 
@@ -28,31 +29,73 @@ namespace Golf.Core.ModelGolf
             Vector3 max = new Vector3(float.MinValue, float.MinValue, float.MinValue);
             List<BoundingBox> boxes = new List<BoundingBox>();
 
-            foreach (ModelMesh mesh in this._model.Model.Meshes)
+            foreach (ModelMesh mesh in this._model.Model.Meshes.Where(elt => elt.Name.Contains("plant")))
             {
-                
-                    foreach (ModelMeshPart meshPart in mesh.MeshParts)
-                    {
-                        int vertexStride = meshPart.VertexBuffer.VertexDeclaration.VertexStride;
-                        int vertexBufferSize = meshPart.NumVertices * vertexStride;
 
-                        int vertexDataSize = vertexBufferSize / sizeof(float);
-                        float[] vertexData = new float[vertexDataSize];
-                        meshPart.VertexBuffer.GetData<float>(vertexData);
-
-                        for (int i = 0; i < vertexDataSize; i += vertexStride / sizeof(float))
-                        {
-                            Vector3 vertex = new Vector3(vertexData[i], vertexData[i + 1], vertexData[i + 2]);
-                            min = Vector3.Min(min, vertex);
-                            max = Vector3.Max(max, vertex);
-                        }
-                        boxes.Add(new BoundingBox(min, max));
-                        min = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue); 
-                        max = new Vector3(float.MinValue, float.MinValue, float.MinValue);
-                    
-                    }
+                var bounding = CreateBoundingBox(_model.Model, mesh);
+                boxes.Add(bounding);
             }
             return boxes;
+        }
+
+        private static BoundingBox CreateBoundingBox(Model model, ModelMesh mesh)
+        {
+
+            Matrix[] boneTransforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(boneTransforms);
+
+            BoundingBox result = new BoundingBox();
+            result.Min = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+            result.Max = new Vector3(float.MinValue, float.MinValue, float.MinValue);
+
+            foreach (ModelMeshPart meshPart in mesh.MeshParts)
+            {
+                BoundingBox? meshPartBoundingBox = GetBoundingBox(meshPart, boneTransforms[mesh.ParentBone.Index]);
+                if (meshPartBoundingBox != null)
+                    result = BoundingBox.CreateMerged(result, meshPartBoundingBox.Value);
+            }
+            result = new BoundingBox(result.Min, result.Max);
+            return result;
+        }
+        private static BoundingBox? GetBoundingBox(ModelMeshPart meshPart, Matrix transform)
+        {
+            if (meshPart.VertexBuffer == null)
+                return null;
+
+            Vector3[] positions = VertexElementExtractor.GetVertexElement(meshPart, VertexElementUsage.Position);
+            if (positions == null)
+                return null;
+
+            Vector3[] transformedPositions = new Vector3[positions.Length];
+            Vector3.Transform(positions, ref transform, transformedPositions);
+
+            for (int i = 0; i < transformedPositions.Length; i++)
+            {
+                Console.WriteLine(" " + transformedPositions[i]);
+            }
+            return BoundingBox.CreateFromPoints(transformedPositions);
+        }
+
+
+        public static class VertexElementExtractor
+        {
+            public static Vector3[] GetVertexElement(ModelMeshPart meshPart, VertexElementUsage usage)
+            {
+                VertexDeclaration vd = meshPart.VertexBuffer.VertexDeclaration;
+                VertexElement[] elements = vd.GetVertexElements();
+
+                Func<VertexElement, bool> elementPredicate = ve => ve.VertexElementUsage == usage && ve.VertexElementFormat == VertexElementFormat.Vector3;
+                if (!elements.Any(elementPredicate))
+                    return null;
+
+                VertexElement element = elements.First(elementPredicate);
+
+                Vector3[] vertexData = new Vector3[meshPart.NumVertices];
+                meshPart.VertexBuffer.GetData((meshPart.VertexOffset * vd.VertexStride) + element.Offset,
+                    vertexData, 0, vertexData.Length, vd.VertexStride);
+
+                return vertexData;
+            }
         }
 
     }
